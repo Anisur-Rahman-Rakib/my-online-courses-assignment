81e1f324b3bcdfc61a9ced8b8b4613862e29ea04
Anisur-Rahman-Rakib <amianis1234567890@gmail.com>
2020-08-29 16:56:42 +0600

    no more

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -6,11 +6,11 @@ const Course = (props) => {
     const { img, title, Trainer, CourseFee } = props.course;
     const handleAddCart = props.handleAddCart;
     return (
-        <div className="d-flex m-2 shadow p-3 mb-5 bg-white rounded p-4">
+        <div className="d-flex m-3 shadow p-2 mb-4 bg-white rounded p-3">
             <div className="d-flex align-items-center">
                 <img src={img} alt="" />
             </div>
-            <div className="ml-3">
+            <div className="ml-4">
                 <h4>{title}</h4>
                 <p><small>Training With <strong>{Trainer}</strong></small></p>
                 <h5>Course Fee : <strong>${CourseFee}</strong></h5>
